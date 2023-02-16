70c346b7e65464bc727cacd01b86de0ef38d3def
Everton Silva <everton.p.silva@outlook.com>
2023-02-15 23:21:21 -0300

    Aplicando os conhecimentos da semana

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+import "./style.css";
+
+function App() {
+  return (
+    <div className="container">
+
+      <header>
+          <h1 className="title">Mesa de trabalho 3</h1>
+      </header>
+
+      <main>
+          <h1 className="title-main">Lorem ipsum</h1>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
+          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
+      </main>
+
+      <footer>
+          <p className="p-footer">Todos os direitos reservados 2022</p>
+      </footer>
+    </div>
+  )
+}
+
+export default App;
