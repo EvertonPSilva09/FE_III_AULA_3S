612383f5f289c77a005fdd9c2b716a2161ef7e97
Everton Silva <everton.p.silva@outlook.com>
2023-02-21 21:17:57 -0300

    Refatorando CSS e estrutura do HTML

--- src/App.jsx
+++ src/App.jsx
@@ -3,21 +3,29 @@ import "./style.css";
 function App() {
   return (
     <div className="container">
-
-      <header>
-          <h1 className="title">Mesa de trabalho 3</h1>
-      </header>
-
-      <main>
-          <h1 className="title-main">Lorem ipsum</h1>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
-          <p>Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.</p>
-      </main>
-
-      <footer>
-          <p className="p-footer">Todos os direitos reservados 2022</p>
-      </footer>
+      <div className="header">
+        <div className="header-title">
+          Mesa de trabalho 3
+        </div>
+      </div>
+      <div className="main">
+        <div className="main-title">
+          Lorem ipsum
+        </div>
+        <div className="lorem">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Quisque vitae lacus ut ligula mattis mattis. Duis lectus libero, efficitur quis posuere eu, volutpat id leo. Cras sed diam ac tortor pulvinar iaculis at id velit. Maecenas eget tellus ut nunc laoreet dictum. Quisque arcu mauris, accumsan sit amet suscipit vitae, sollicitudin at leo.
+        </div>
+        <div className="integrantes">
+        Everton Silva, Gabriella Faria, Mateus Silva, Sabrina Freiberger, Victor Pessoa
+        </div>
+      </div>
+      <div className="footer">
+        <div className="footer-title">
+          Todos os direitos reservados 2022
+        </div>
+      </div>
     </div>
   )
 }
